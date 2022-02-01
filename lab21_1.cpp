70eb144b7543d671e08d51f85de224c46e8a150f
tanawat640610678 <thanawat_chai@cmu.ac.th>
2022-02-01 21:15:11 +0700

    Lab21_1.cpp Complete

--- lab21_1.cpp
+++ lab21_1.cpp
@@ -5,8 +5,26 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
+double overlap( Rect R1 , Rect R2 ){
 
-
+	double x1 , x2 , y1 , y2 , area;
+	if(R2.x > R1.x && R1.x + R1.w < R2.x)    return 0;
+	
+	if(R2.x < R1.x && R2.x + R2.w < R1.x)    return 0;
+	
+	if(R1.x <= R2.x)   x1 = R2.x;
+	else  x1 = R1.x; 
+	
+	if(R1.x + R1.w <= R2.x + R2.w)  x2 = R1.x + R1.w;
+	else  x2 = R2.x + R2.w;
+	
+	if(R1.y <= R2.y)   y1 = R1.y;
+	else  y1 = R2.y;
+	 
+	if(R1.y - R1.h >= R2.y - R2.h)  y2 = R1.y - R1.h;
+	else  y2 = R2.y - R2.h;
+	
+    area = abs((x2-x1)*(y2-y1));
+	return area;		
 
 }
